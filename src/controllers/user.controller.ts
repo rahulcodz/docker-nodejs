08c7512bca77be41d80a30342e4af268624af433
Rahul Panchal <127726141+rahulcodz@users.noreply.github.com>
2023-09-21 17:21:07 +0530

    misc

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,9 +1,29 @@
 import { Request, Response } from "express"
 
-export const registerUser = async (req: Request, res: Response) => {
-  res.json("sdasasd")
-  console.log("object")
+class UserController {
+  async registerUser(req: Request, res: Response) {
+    const person = new Person("Rahul", 20)
+    res.json(person)
+  }
+
+  async loginUser(req: Request, res: Response) {
+    const person = new Person("Rahul", 20)
+    res.json(person)
+  }
 }
-export const loginUser = () => {}
-export const logoutUser = () => {}
-export const deleteUser = () => {}
+class Person {
+  name: String
+  age: Number
+  constructor(name: String, age: Number) {
+    this.name = name
+    this.age = age
+  }
+
+  sayHello() {
+    console.log(
+      `Hello, my name is ${this.name} and I am ${this.age} years old.`
+    )
+  }
+}
+
+export const userController = new UserController()
